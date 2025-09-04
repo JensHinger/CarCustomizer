bf192f13f9a3296bc75635af26d447b4a253bb53
JensHinger <hinger.jens@gmail.com>
2025-09-04 14:08:49 +0200

    Testing CI

--- car-customizer-frontend/src/app/page.tsx
+++ car-customizer-frontend/src/app/page.tsx
@@ -7,6 +7,7 @@ export default async function Page() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>Car Brand Landing Page</h1>
+        <p>Great Cars</p>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
        
